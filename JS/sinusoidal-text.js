9b07c4ded3ad1edff47c758350d5386badf45396
tiagodacosta1 <tiagocosta4823@gmail.com>
2023-04-16 17:47:27 +0100

    Update email section, circle text, sinusoidal text

---
+++ JS/sinusoidal-text.js
@@ -0,0 +1,25 @@
+var textPath = document.querySelector('#sinusoidal-text');
+
+var textContainer = document.querySelector('#sinusoidal');
+
+var path = document.querySelector( textPath.getAttribute('href') );
+
+var pathLength = path.getTotalLength();
+console.log(pathLength);
+
+function updateTextPathOffset(offset){
+  textPath.setAttribute('startOffset', offset); 
+}
+
+updateTextPathOffset(pathLength);
+
+function onScroll(){
+  requestAnimationFrame(function(){
+    var rect = textContainer.getBoundingClientRect();
+    var scrollPercent = rect.y / window.innerHeight;
+    console.log(scrollPercent);
+    updateTextPathOffset( scrollPercent * 2 * pathLength );
+  });
+}
+
+window.addEventListener('scroll',onScroll);
