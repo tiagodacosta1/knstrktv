619d3183c6b52888b6f10ab5cde862b09ee05794
snikfi <39119391+snikfi@users.noreply.github.com>
2023-06-10 01:36:04 +0100

    mobile updates portfolio

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,88 +1,88 @@
-$(document).ready(function() {
-  $('html, body').scrollTop(0);
+// $(document).ready(function() {
+//   $('html, body').scrollTop(0);
 
-  (function($) {
-    var selector = ".viewport";
+//   (function($) {
+//     var selector = ".viewport";
 
-    var $slides = $(selector);
+//     var $slides = $(selector);
 
-    var currentSlide = 0;
-    var isAnimating = false;
+//     var currentSlide = 0;
+//     var isAnimating = false;
 
-    var stopAnimation = function() {
-      setTimeout(function() {
-        isAnimating = false;
-      }, 300);
-    };
+//     var stopAnimation = function() {
+//       setTimeout(function() {
+//         isAnimating = false;
+//       }, 300);
+//     };
 
-    var bottomIsReached = function($elem) {
-      var rect = $elem[0].getBoundingClientRect();
-      return rect.bottom <= $(window).height();
-    };
+//     var bottomIsReached = function($elem) {
+//       var rect = $elem[0].getBoundingClientRect();
+//       return rect.bottom <= $(window).height();
+//     };
 
-    var topIsReached = function($elem) {
-      var rect = $elem[0].getBoundingClientRect();
-      return rect.top >= 0;
-    };
+//     var topIsReached = function($elem) {
+//       var rect = $elem[0].getBoundingClientRect();
+//       return rect.top >= 0;
+//     };
 
-    console.log("$slides:", $slides);
-    console.log("currentSlide:", currentSlide);
-    console.log("isAnimating:", isAnimating);
+//     console.log("$slides:", $slides);
+//     console.log("currentSlide:", currentSlide);
+//     console.log("isAnimating:", isAnimating);
 
-    document.addEventListener(
-      "wheel",
-      function(event) {
-        console.log("Wheel event triggered");
+//     document.addEventListener(
+//       "wheel",
+//       function(event) {
+//         console.log("Wheel event triggered");
 
-        var $currentSlide = $($slides[currentSlide]);
+//         var $currentSlide = $($slides[currentSlide]);
 
-        if (isAnimating) {
-          event.preventDefault();
-          return;
-        }
+//         if (isAnimating) {
+//           event.preventDefault();
+//           return;
+//         }
 
-        var direction = -event.deltaY;
-        console.log("Direction:", direction);
+//         var direction = -event.deltaY;
+//         console.log("Direction:", direction);
 
-        if (direction < 0) {
-          console.log("Scrolling to the next slide");
-          if (currentSlide + 1 >= $slides.length) return;
-          if (!bottomIsReached($currentSlide)) return;
-          event.preventDefault();
-          currentSlide++;
-          var $slide = $($slides[currentSlide]);
-          var offsetTop = $slide.offset().top;
-          isAnimating = true;
-          console.log("Animation started");
-          $("html, body").animate(
-            {
-              scrollTop: offsetTop
-            },
-            350,
-            stopAnimation
-          );
-          console.log("Animation stopped");
-        } else {
-          console.log("Scrolling to the previous slide");
-          if (currentSlide - 1 < 0) return;
-          if (!topIsReached($currentSlide)) return;
-          event.preventDefault();
-          currentSlide--;
-          var $slide = $($slides[currentSlide]);
-          var offsetTop = $slide.offset().top;
-          isAnimating = true;
-          console.log("Animation started");
-          $("html, body").animate(
-            {
-              scrollTop: offsetTop
-            },
-            0,
-            stopAnimation
-          );
-          console.log("Animation stopped");
-        }
-      },
-      { passive: false }
-    );
-  })(jQuery);
-});
+//         if (direction < 0) {
+//           console.log("Scrolling to the next slide");
+//           if (currentSlide + 1 >= $slides.length) return;
+//           if (!bottomIsReached($currentSlide)) return;
+//           event.preventDefault();
+//           currentSlide++;
+//           var $slide = $($slides[currentSlide]);
+//           var offsetTop = $slide.offset().top;
+//           isAnimating = true;
+//           console.log("Animation started");
+//           $("html, body").animate(
+//             {
+//               scrollTop: offsetTop
+//             },
+//             350,
+//             stopAnimation
+//           );
+//           console.log("Animation stopped");
+//         } else {
+//           console.log("Scrolling to the previous slide");
+//           if (currentSlide - 1 < 0) return;
+//           if (!topIsReached($currentSlide)) return;
+//           event.preventDefault();
+//           currentSlide--;
+//           var $slide = $($slides[currentSlide]);
+//           var offsetTop = $slide.offset().top;
+//           isAnimating = true;
+//           console.log("Animation started");
+//           $("html, body").animate(
+//             {
+//               scrollTop: offsetTop
+//             },
+//             0,
+//             stopAnimation
+//           );
+//           console.log("Animation stopped");
+//         }
+//       },
+//       { passive: false }
+//     );
+//   })(jQuery);
+// });
